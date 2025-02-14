65437937311d449561e4fecfa34ade7fc1f8f5dd
shivam260598 <46588584+shivam260598@users.noreply.github.com>
2025-02-14 18:21:36 +0000

    mobile view implemented and .gitignore file added

--- src/components/InventoryTable.tsx
+++ src/components/InventoryTable.tsx
@@ -24,73 +24,74 @@ export const InventoryTable: React.FC<InventoryTableProps> = ({
 
     return (
         <React.Fragment>
-            <table className="inventory-table">
-                <thead>
-                    <tr>
-                        <th>
-                            <div className="header-container">Name</div>
-                        </th>
-                        <th>
-                            <div className="header-container">Category</div>
-                        </th>
-                        <th>
-                            <div className="header-container">Price</div>
-                        </th>
-                        <th>
-                            <div className="header-container">Quantity</div>
-                        </th>
-                        <th>
-                            <div className="header-container">Value</div>
-                        </th>
-                        <th>
-                            <div className="header-container">ACTION</div>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {products.map((product) => {
-                        const price = parseFloat(product.price.replace('$', ''));
-                        return (
-                            <tr
-                                key={product.name}
-                                className={product.disabled ? 'disabled-row' : ''}
-                            >
-                                <td>{product.name}</td>
-                                <td>{product.category}</td>
-                                <td>{price.toFixed(2) || 0}</td>
-                                <td>{product.quantity}</td>
-                                <td>${(product.quantity * price).toFixed(2)}</td>
-                                <td>
-                                    <button
-                                        className="action-button edit-wrapper"
-                                        onClick={() => onEdit?.(product)}
-                                        disabled={product.disabled || !userView}
-                                    >
-                                        <Edit style={{ width: 18, height: 18 }} />
-                                    </button>
-                                    <button
-                                        disabled={!userView}
-                                        className="action-button disable-wrapper"
-                                        onClick={() => onToggleStatus?.(product.name)}
-                                    >
-                                        {product?.disabled ?
-                                            <VisibilityOff style={{ width: 18, height: 18 }} />
-                                            : <Visibility style={{ width: 18, height: 18 }} />}
-                                    </button>
-                                    <button
-                                        disabled={!userView}
-                                        className="action-button delete-wrapper"
-                                        onClick={() => onDelete?.(product.name)}
-                                    >
-                                        <Delete style={{ width: 18, height: 18 }} />
-                                    </button>
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
-
+            <div className="table-responsive">
+                <table className="inventory-table">
+                    <thead>
+                        <tr>
+                            <th>
+                                <div className="header-container">Name</div>
+                            </th>
+                            <th>
+                                <div className="header-container">Category</div>
+                            </th>
+                            <th>
+                                <div className="header-container">Price</div>
+                            </th>
+                            <th>
+                                <div className="header-container">Quantity</div>
+                            </th>
+                            <th>
+                                <div className="header-container">Value</div>
+                            </th>
+                            <th>
+                                <div className="header-container">ACTION</div>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((product) => {
+                            const price = parseFloat(product.price.replace('$', ''));
+                            return (
+                                <tr
+                                    key={product.name}
+                                    className={product.disabled ? 'disabled-row' : ''}
+                                >
+                                    <td>{product.name}</td>
+                                    <td>{product.category}</td>
+                                    <td>{price.toFixed(2) || 0}</td>
+                                    <td>{product.quantity}</td>
+                                    <td>${(product.quantity * price).toFixed(2)}</td>
+                                    <td>
+                                        <button
+                                            className="action-button edit-wrapper"
+                                            onClick={() => onEdit?.(product)}
+                                            disabled={product.disabled || userView}
+                                        >
+                                            <Edit style={{ width: 18, height: 18 }} />
+                                        </button>
+                                        <button
+                                            disabled={userView}
+                                            className="action-button disable-wrapper"
+                                            onClick={() => onToggleStatus?.(product.name)}
+                                        >
+                                            {product?.disabled ?
+                                                <VisibilityOff style={{ width: 18, height: 18 }} />
+                                                : <Visibility style={{ width: 18, height: 18 }} />}
+                                        </button>
+                                        <button
+                                            disabled={userView}
+                                            className="action-button delete-wrapper"
+                                            onClick={() => onDelete?.(product.name)}
+                                        >
+                                            <Delete style={{ width: 18, height: 18 }} />
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
             {(products || []).length === 0 ? <div className="no-data-found">
                 No Data Found
             </div> : null}
